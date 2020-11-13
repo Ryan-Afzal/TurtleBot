78febedf05ebe792fe1e6e93049a705979046afe
Ryan-Afzal <ryan.afzal@outlook.com>
2020-11-13 14:50:53 -0800

    Tried to get JVM running to work.
    It worked before, but now encountering an issue with Admin permissions and Win32 access

--- TurtleBot/TurtleBot-App-UWP/MainPage.xaml.cs
+++ TurtleBot/TurtleBot-App-UWP/MainPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -18,39 +20,97 @@ using Windows.UI.Xaml.Navigation;
 namespace TurtleBot_App_UWP {
     public sealed partial class MainPage : Page {
 
-        public static readonly string JAVAPATH = @"C:\Users\ryana\Documents\Test\";
-        public static readonly string CODEPATH = @"Main.java";
-        public static readonly string FILEPATH = @"Main.class";
+        private string sourcePath;
+        private string javaPath;
+
+        private Process javacProcess;
+        private Process jarProcess;
 
         public MainPage() {
 			this.InitializeComponent();
+
+            this.sourcePath = null;
+            this.javaPath = null;
+
+            this.javacProcess = null;
+            this.jarProcess = null;
+
+            JarRunButton.IsEnabled = false;
+            JarKillButton.IsEnabled = false;
         }
 
-        private async void RunButton_Click(object sender, RoutedEventArgs e) {
-            try {
-                Debug.WriteLine("RunButton Clicked!");
+        private void DisposeJarProcess(object sender, System.EventArgs e) {
+            Debug.WriteLine("Disposing JVM...");
+            this.jarProcess.Dispose();
+            this.jarProcess = null;
 
-                string input_path = JAVAPATH + CODEPATH;
-                string output_path = JAVAPATH + FILEPATH;
+            JarRunButton.IsEnabled = true;
+            JarKillButton.IsEnabled = false;
+            Debug.WriteLine("Disposed JVM.");
+		}
 
-                Debug.WriteLine("Compiling Code...");
+        /// <summary>
+        /// Picks a <c>.class</c> or <c>.jar</c> file for the JVM to run.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+		private async void JarPickButton_Click(object sender, RoutedEventArgs e) {
+			// Create a file picker and give it properties
+            FileOpenPicker picker = new FileOpenPicker() {
+				ViewMode = PickerViewMode.List,
+				SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+			};
 
-                using (Process compiler = new Process()) {
-                    compiler.StartInfo.UseShellExecute = false;
-                    compiler.StartInfo.FileName = "C:\\\"Program Files\"\\Java\\jdk1.8.0_201\\bin\\javac";
-                    compiler.StartInfo.CreateNoWindow = true;
-                    compiler.StartInfo.Arguments = input_path;
-                    compiler.Start();
-                    compiler.WaitForExit();
-                }
+            // Add filters
+			picker.FileTypeFilter.Add(".class");
+            picker.FileTypeFilter.Add(".jar");
+            picker.FileTypeFilter.Add(".java");
 
-                Debug.WriteLine("Done Compiling!");
+            // Pick File
+            Debug.WriteLine("Picking file...");
+            StorageFile pickedFile = await picker.PickSingleFileAsync();
 
-                //Debug.WriteLine("Running...");
-                //Debug.WriteLine("Done!");
-            } catch (Exception ex) {
-                Debug.WriteLine(ex.Message);
+            if (pickedFile is null) {
+                // If the file isn't valid, real, or the user closed the dialog, clear the paths and disable the buttons
+                this.javaPath = null;
+                JarPickLabel.Text = "N/A";
+
+                JarRunButton.IsEnabled = false;
+                JarKillButton.IsEnabled = false;
+
+                Debug.WriteLine($"Couldn't pick file: File was null");
+            } else {
+                // If the file is real, set the paths to it and enable the buttons
+                this.javaPath = pickedFile.Path;
+                JarPickLabel.Text = pickedFile.Path;
+
+                JarRunButton.IsEnabled = true;
+                JarKillButton.IsEnabled = false;
+
+                Debug.WriteLine($"Picked file: {this.javaPath}");
             }
+        }
+
+		private void JarRunButton_Click(object sender, RoutedEventArgs e) {
+            // Run the JVM with the picked file
+
+            Debug.WriteLine("Starting JVM...");
+            this.jarProcess = new Process();
+            this.jarProcess.StartInfo.UseShellExecute = false;
+            this.jarProcess.StartInfo.FileName = "C:\\\"Program Files\"\\Java\\jdk1.8.0_201\\bin\\java";
+            this.jarProcess.StartInfo.Arguments = this.javaPath;
+            this.jarProcess.EnableRaisingEvents = true;
+            this.jarProcess.Exited += this.DisposeJarProcess;
+            this.jarProcess.Start();
+            Debug.WriteLine("JVM Started.");
+        }
+
+		private void JarKillButton_Click(object sender, RoutedEventArgs e) {
+            // Kill the JVM process and dispose of it
+            Debug.WriteLine("Killing JVM...");
+            this.jarProcess.Kill();
+            this.DisposeJarProcess(null, null);
+            Debug.WriteLine("Killed JVM.");
         }
 
     }
--- TurtleBot/TurtleBot-Robot/Program.cs
+++ TurtleBot/TurtleBot-Robot/Program.cs
@@ -1,11 +1,57 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace TurtleBot_Robot {
 	public class Program {
 		public static async Task Main(string[] args) {
-			await Task.Delay(1000);
-			Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.JarRunButton_Click();
 		}
-	}
+
+		private string javaPath;
+		private Process jarProcess;
+
+		public Program() {
+			this.javaPath = @"C:\Users\ryana\Documents\Test\Main.class";
+			this.jarProcess = null;
+		}
+
+        private void DisposeJarProcess(object sender, EventArgs e) {
+            Debug.WriteLine("Disposing JVM...");
+            Console.WriteLine("Disposing JVM...");
+            this.jarProcess.Dispose();
+            this.jarProcess = null;
+            Debug.WriteLine("Disposed JVM.");
+            Console.WriteLine("Disposed JVM.");
+        }
+
+        private void JarRunButton_Click() {
+            // Run the JVM with the picked file
+
+            Debug.WriteLine("Starting JVM...");
+            Console.WriteLine("Starting JVM...");
+            this.jarProcess = new Process();
+            this.jarProcess.StartInfo.UseShellExecute = false;
+            this.jarProcess.StartInfo.FileName = "C:\\\"Program Files\"\\Java\\jdk1.8.0_201\\bin\\java.exe";
+            this.jarProcess.StartInfo.Arguments = this.javaPath;
+            //this.jarProcess.StartInfo.Verb = "runas";
+            //this.jarProcess.StartInfo.UseShellExecute = true;
+            this.jarProcess.EnableRaisingEvents = true;
+            this.jarProcess.Exited += this.DisposeJarProcess;
+            this.jarProcess.Start();
+            Debug.WriteLine("JVM Started.");
+            Console.WriteLine("JVM Started.");
+        }
+
+        private void JarKillButton_Click() {
+            // Kill the JVM process and dispose of it
+            Debug.WriteLine("Killing JVM...");
+            Console.WriteLine("Killing JVM...");
+            this.jarProcess.Kill();
+            this.DisposeJarProcess(null, null);
+            Debug.WriteLine("Killed JVM.");
+            Console.WriteLine("Killed JVM.");
+        }
+    }
 }
